1a0211eabaee480e129a0c494f7e6ee6be870457
Yijiong Rong <yijiong.rong@mckesson.com>
2018-12-28 11:20:52 -0700

    add comments

--- nettyhelloworld/src/main/java/Server.java
+++ nettyhelloworld/src/main/java/Server.java
@@ -2,6 +2,7 @@ import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
@@ -10,21 +11,24 @@ public class Server {
 
   public static void main(String[] args) throws InterruptedException {
 
-    NioEventLoopGroup boss = new NioEventLoopGroup();
-    NioEventLoopGroup worker = new NioEventLoopGroup();
+    // handler request from client
+    EventLoopGroup boss = new NioEventLoopGroup();
+    // process request from client
+    EventLoopGroup worker = new NioEventLoopGroup();
 
     ServerBootstrap serverBootstrap = new ServerBootstrap();
     serverBootstrap
         .group(boss, worker)
-        .channel(NioServerSocketChannel.class)
-        .option(ChannelOption.SO_BACKLOG, 1024)
-        .childOption(ChannelOption.SO_SNDBUF, 32 * 1024)
-        .option(ChannelOption.SO_RCVBUF, 32 * 1024)
-        .childOption(ChannelOption.SO_KEEPALIVE, true)
+        .channel(NioServerSocketChannel.class)  // server mode
+        .option(ChannelOption.SO_BACKLOG, 1024)  // tcp buffer
+        .childOption(ChannelOption.SO_SNDBUF, 32 * 1024)  // sender buffer size
+        .option(ChannelOption.SO_RCVBUF, 32 * 1024)  // receiver buffer size
+        .childOption(ChannelOption.SO_KEEPALIVE, true)  // keep connection alive
         .childHandler(new ChannelInitializer<SocketChannel>() {
 
           @Override
           protected void initChannel(SocketChannel channel) {
+            // use server handler to process request
             channel.pipeline().addLast(new ServerHandler());
           }
         });
